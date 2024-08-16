56ea4e289a5dd309ee261c4cb2b8bf2f4f218c0d
jt151077 <jt151077@gmail.com>
2024-08-16 10:16:08 +0000

    removed unecessary code for VPC and Firewall

--- network.tf
+++ network.tf
@@ -15,41 +15,6 @@
  */
 
 
-resource "google_compute_network" "custom-vpc" {
-  name                    = "custom-vpc"
-  project                 = var.project_id
-  auto_create_subnetworks = false
-}
-
-resource "google_compute_subnetwork" "custom-subnet" {
-  name          = "custom-subnet"
-  project       = var.project_id
-  ip_cidr_range = "10.240.0.0/24"
-  region        = var.project_default_region
-  network       = google_compute_network.custom-vpc.id
-
-  log_config {
-    aggregation_interval = "INTERVAL_10_MIN"
-    flow_sampling        = 0.5
-    metadata             = "INCLUDE_ALL_METADATA"
-  }
-}
-
-resource "google_compute_firewall" "custom-subnet-fwr-ssh" {
-  name    = "allow-ingress-from-iap"
-  project = var.project_id
-  network = google_compute_network.custom-vpc.id
-
-  source_ranges = [
-    "35.235.240.0/20",
-  ]
-
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-}
-
 # LB with https (http redirect to https)
 resource "google_compute_target_http_proxy" "default" {
   project = var.project_id
@@ -121,45 +86,6 @@ resource "google_compute_global_address" "default" {
   project    = var.project_id
   name       = "${var.project_id}-address"
   ip_version = "IPV4"
-}
-
-resource "google_compute_router" "router" {
-  name    = "router"
-  project = var.project_id
-  region  = var.project_default_region
-  network = google_compute_network.custom-vpc.id
-}
-
-resource "google_compute_router_nat" "nat" {
-  name    = "nat"
-  project = var.project_id
-  region  = var.project_default_region
-  router  = google_compute_router.router.name
-
-  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
-  subnetwork {
-    name                    = google_compute_subnetwork.custom-subnet.id
-    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
-  }
-
-  nat_ip_allocate_option = "AUTO_ONLY"
-}
-
-resource "google_compute_firewall" "default" {
-  project = var.project_id
-  name    = "allow-iap-traffic"
-  network = google_compute_network.custom-vpc.id
-
-  #tfsec:ignore:google-compute-no-public-ingress
-  source_ranges = [
-    "130.211.0.0/22",
-    "35.191.0.0/16"
-  ]
-
-  allow {
-    protocol = "tcp"
-    ports    = ["80"]
-  }
 }
 
 resource "google_iap_brand" "project_brand" {
